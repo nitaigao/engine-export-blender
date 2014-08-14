31a4c95bed31495bceceead42703a617766d2ced
Nicholas Kostelnik <nkostelnik@gmail.com>
2014-08-14 16:00:11 +0100

    rotate bones on export

--- export_json.py
+++ export_json.py
@@ -1,17 +1,44 @@
+#bpy.data.objects[3].to_mesh(bpy.context.scene, True, 'RENDER').vertices[0].groups[0].group
+#bpy.data.objects[3].vertex_groups[0].name
+
 import bpy
 import math
 import mathutils
 import json
 
 def do_export(filepath):
+
+  X_ROT = mathutils.Matrix.Rotation(-math.pi/2, 4, 'X')
+  
+  armatures_data = []
+  for armature in bpy.data.armatures:
+    bones_data = []
+    for bone in armature.bones:
+      translation = bone.head
+      matrix = X_ROT.to_3x3() * bone.matrix
+      scale = matrix.to_scale()
+      orientation = matrix.to_quaternion()
+      bones_data.append({
+        "name" : bone.name, 
+        "scale" : {"x" : scale.x, "y" : scale.y, "z" : scale.z},
+        "translation" : {"x" : translation.x, "y" : translation.y, "z" : translation.z},
+        "orientation" : {"x" : orientation.x, "y" : orientation.y, "z" : orientation.z, "w" : orientation.w }
+      })
+    armatures_data.append({"name" : armature.name, "bones" : bones_data})
+
   submeshes_data = []
-  for ob in bpy.data.objects:
-      if ob.type == 'MESH':
-          mesh = ob.to_mesh(bpy.context.scene, True, 'RENDER')
-          X_ROT = mathutils.Matrix.Rotation(-math.pi/2, 4, 'X')
+  for obj in bpy.data.objects:
+      if obj.type == 'MESH':
+          mesh = obj.to_mesh(bpy.context.scene, True, 'RENDER')
 
           vertices = []
           normals = []
+          weights = []
+          vertex_groups = []
+
+          for group in obj.vertex_groups:
+            vertex_groups.append({"name":group.name,"index":group.index})
+          
           for vertex in mesh.vertices:
               v = X_ROT * vertex.co
               vertices.append(v.x)
@@ -24,6 +51,11 @@ def do_export(filepath):
               normals.append(n.x)
               normals.append(n.y)
               normals.append(n.z)
+
+              vertex_weights = []
+              for group in vertex.groups:
+                vertex_weights.append({"index":group.group, "weight":group.weight})
+              weights.append(vertex_weights)
 
           indices = []
           for polygon in mesh.polygons:
@@ -49,10 +81,22 @@ def do_export(filepath):
                   indices.append(b)
                   indices.append(c)
 
-          submesh_data = {"vertices":vertices, "normals":normals, "indices":indices}
+          armature = ""
+
+          if obj.parent and obj.parent.type == 'ARMATURE':
+            armature = obj.parent.name
+
+          submesh_data = {
+            "armature":armature, 
+            "vertices":vertices, 
+            "normals":normals, 
+            "indices":indices, 
+            "weights":weights,
+            "groups":vertex_groups
+          }
           submeshes_data.append(submesh_data)
 
-  mesh_data = {"submeshes" : submeshes_data}
+  mesh_data = {"submeshes" : submeshes_data, "armatures" : armatures_data}
   content = json.dumps(mesh_data, sort_keys=True, indent=4, separators=(',', ': '))
   out = open(filepath, "w", encoding="utf-8")
   out.write(content)
